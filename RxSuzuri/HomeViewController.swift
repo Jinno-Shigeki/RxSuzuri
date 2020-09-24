383978a2f5dd4442fd9706787131dc34dcdf515a
jinn <booboo080yamaya@gmail.com>
2020-09-24 12:26:25 +0900

    first commit

---
+++ RxSuzuri/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RxSuzuri
+//
+//  Created by 神野成紀 on 2020/09/24.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ RxSuzuri/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  RxSuzuri
+//
+//  Created by 神野成紀 on 2020/09/24.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import Foundation
